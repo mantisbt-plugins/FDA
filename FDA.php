117a77cf8be4f316d94fb8d8ee9692f42b4b5cd4
CasN <cas@nuy.info>
2023-03-16 16:31:05 +0100

    Update FDA.php

--- FDA.php
+++ FDA.php
@@ -8,7 +8,7 @@ class FDAPlugin extends MantisPlugin {
 		$this->requires    = array('MantisCore'       => '2.0.0',);
 		$this->author      = 'Cas Nuy';
 		$this->contact     = 'Cas-at-nuy.info';
-		$this->url         = 'http://www.nuy.info';
+		$this->url         = 'https://github.com/mantisbt-plugins/FDAo';
 	}
 
 	function init() { 
